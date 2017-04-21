d8ec04ce12b7e060b18ffc513a5354e8bf03ff72
Janven <janvenzhao@gmail.com>
2017-04-21 11:49:38 +0800

    初始化

---
+++ ZJPodTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJPodTest
+//
+//  Created by Janven on 17/4/21.
+//  Copyright © 2017年 Janven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
